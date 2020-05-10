18837a1dca576d0aae68b081edcea8023d7f7b5e
Ekem Herve <ekemherve@gmail.com>
2020-05-10 10:00:20 +0200

    Initial project

---
+++ src/main/java/com/example/unittestservicelayermockito/UnitTestServiceLayerMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.unittestservicelayermockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestServiceLayerMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestServiceLayerMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/unittestservicelayermockito/UnitTestServiceLayerMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unittestservicelayermockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestServiceLayerMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
